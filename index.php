ba64c3f20c0303875d9956fdcb83f73a91d58f03
134ashwin <135332366+134ashwin@users.noreply.github.com>
2024-03-14 03:06:12 -0700

    added index.php

---
+++ index.php
@@ -0,0 +1,221 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="css/style.css" class="css">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+  <title>Hello, world!</title>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Banshia Jwellery</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+
+        <li class="nav-item ">
+          <a class="nav-link" href="#">Services<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.php">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contactUs.php">Contacts</a>
+        </li>
+
+
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+  <!-- Carousel Start Here  -->
+
+  <div id="demo" class="carousel slide" data-ride="carousel">
+
+    <!-- Indicators -->
+    <ul class="carousel-indicators">
+      <li data-target="#demo" data-slide-to="0" class="active"></li>
+      <li data-target="#demo" data-slide-to="1"></li>
+      <li data-target="#demo" data-slide-to="2"></li>
+    </ul>
+
+    <!-- The slideshow -->
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="img/c1.jpg" alt="Los Angeles" width="1100" height="500">
+      </div>
+      <div class="carousel-item">
+        <img src="img/c2.jpg" alt="Chicago" width="1100" height="500">
+      </div>
+      <div class="carousel-item">
+        <img src="img/c3.jpg" alt="New York" width="1100" height="500">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+      <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#demo" data-slide="next">
+      <span class="carousel-control-next-icon"></span>
+    </a>
+  </div>
+
+  <section class="my-5">
+    <div class="py-5">
+      <h2 class="text-center">About Us</h2>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-12">
+          <img src="img/c2.jpg" alt="" class="img-fluid aboutimg">
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-12 text-center">
+          <h2 class="display-4">Who We are</h2>
+          <p class="py-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum consectetur earum quae accusantium adipisci dolorem nobis exercitationem dolor officia quos eaque fugit nam praesentium temporibus nisi eius iusto beatae ipsum odit, tempore delectus nulla! Aliquid voluptas a fuga obcaecati incidunt laudantium fugit mollitia, optio magnam illo omnis. Beatae perferendis fuga atque at dicta hic quod sequi qui minus libero ducimus aut animi iste magnam accusantium, optio illum amet ea. Nemo amet voluptatum distinctio laudantium deserunt atque vel eum minus autem.</p>
+          <a href="" class="btn btn-success">Check More</a>
+        </div>
+      </div>
+    </div>
+
+
+  </section>
+
+
+
+  <section class="my-5">
+    <div class="py-5">
+      <h2 class="text-center"> Our Services </h2>
+    </div>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-4 col-md-4 col-12">
+          <div class="card" >
+            <img class="card-img-top" src="img/image1.jpg" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">AAAAA1</h4>
+              <p class="card-text">Some example text.</p>
+              <a href="#" class="btn btn-primary">See Profile</a>
+            </div>
+          </div>
+        </div>
+
+        <!-- card 2  -->
+        <div class="col-lg-4 col-md-4 col-12">
+          <div class="card" >
+            <img class="card-img-top" src="img/image1.jpg" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">AAAAA1</h4>
+              <p class="card-text">Some example text.</p>
+              <a href="#" class="btn btn-primary">See Profile</a>
+            </div>
+          </div>
+        </div>
+
+        <!-- card- 3  -->
+        <div class="col-lg-4 col-md-4 col-12">
+          <div class="card" >
+            <img class="card-img-top" src="img/image1.jpg" alt="Card image">
+            <div class="card-body">
+              <h4 class="card-title">AAAAA1</h4>
+              <p class="card-text">Some example text.</p>
+              <a href="#" class="btn btn-primary">See Profile</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="my-5">
+    <div class="py-5">
+      <h2 class="text-center">Gallery</h2>
+    </div>
+    <div class="container">
+      <div class="row">
+          <div class="col-lg-4 col-md-4 col-12">
+  <img src="img/image4.jpg" alt="" class="img-fluid pb-4">
+          </div>
+          <div class="col-lg-4 col-md-4 col-12">
+  <img src="img/image4.jpg" alt="" class="img-fluid pb-4">
+          </div>
+          <div class="col-lg-4 col-md-4 col-12">
+  <img src="img/image4.jpg" alt="" class="img-fluid pb-4">
+          </div>
+          <div class="col-lg-4 col-md-4 col-12">
+  <img src="img/image4.jpg" alt="" class="img-fluid pb-4">
+          </div>
+          <div class="col-lg-4 col-md-4 col-12">
+  <img src="img/image4.jpg" alt="" class="img-fluid pb-4">
+          </div>
+          <div class="col-lg-4 col-md-4 col-12">
+  <img src="img/image4.jpg" alt="" class="img-fluid pb-4">
+          </div>
+      </div>
+    </div>
+
+  </section>
+
+
+  <section class="my-5">
+    <div class="py-5">
+      <h2 class="text-center">Contact Us</h2>
+    </div>
+
+    <div class="w-50 m-auto">
+      <form action="userinfo.php" method="post">
+        <div class="form-group">
+          <label for="">Username</label>
+          <input type="text"  name = "user" autocomplete="off" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label for="">Email Id</label>
+          <input type="text"  name = "email" autocomplete="off" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label for="">Mobile</label>
+          <input type="text"  name = "mobile" autocomplete="off" class="form-control">
+        </div>
+
+        <div class="form-group">
+          <label for="">Comments</label>
+         <textarea name="comments" id="" class="form-control" rows="10" ></textarea>
+        </div>
+        <button  type="submit" name="submit" class="btn btn-success">Submit</button>
+
+      </form>
+    </div>
+  </section>
+
+  <footer>
+    <p class="p-5 bg-dark text-white text-center">@Banshia.com</p>
+  </footer>
+
+
+
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
+</html>
